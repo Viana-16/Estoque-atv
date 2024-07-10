11820700ac9f6d752e381011379ba6e64fb2c260
hugoleonardo211 <163910372+Viana-16@users.noreply.github.com>
2024-07-10 13:52:45 -0300

    update

--- Estoque atv/Program.cs
+++ Estoque atv/Program.cs
@@ -1,9 +1,23 @@
-﻿using Estoque_atv;
+﻿using Atividade;
+using Estoque_atv;
 
 
 //exer1 e = new exer1();
 //e.CalcularEstoque(10,20);
 
-exer2 v = new exer2();
-v.Convercao();
+//exer2 v = new exer2();
+//v.Convercao();
+
+//Exer4 e = new Exer4();
+//e.TrocarValores();
+
+//Exer5 f = new Exer5();
+//f.Modulo();
+
+//Exer6 p= new Exer6();
+//p.EscolheOperacao();
+
+Exer7 g = new Exer7();
+g.RealizarAcao();         
+
 
